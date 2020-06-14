c9aa2d4621a71f636da5460a9cc39c8e00f99feb
Александр Денисович Петров <sanchpet@niuitmo.ru>
2020-06-14 11:52:44 +0300

    a groundwork for connection with SQL-database

---
+++ DB.cs
@@ -0,0 +1,33 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SQLFirstTutorial
+{
+    class DB
+    {
+        MySqlConnection connection = new MySqlConnection("server=localhost;port=3306;username=root;password=root;database=sqltutorial");
+
+        public void OpenConnection()
+        {
+            if(connection.State == System.Data.ConnectionState.Closed)
+            {
+                connection.Open();
+            }
+        }
+
+        public void CloseConnection()
+        {
+            if (connection.State == System.Data.ConnectionState.Open)
+            {
+                connection.Close();
+            }
+        }
+
+        public MySqlConnection GetConnection()
+        {
+            return connection;
+        }
+    }
+}
